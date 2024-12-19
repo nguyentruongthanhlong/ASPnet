312be4945c41226aa1a5305705321e2984d442e2
nguyentruongthanhlong <thanhlong251004@gmail.com>
2024-12-20 00:00:21 +0700

    thanhlongasp

---
+++ nguyentruongthanhlong/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nguyentruongthanhlong.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Category()
+        {
+            return View();
+        }
+    }
+}
---
+++ nguyentruongthanhlong/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="nguyentruongthanhlong.MvcApplication" Language="C#" %>
